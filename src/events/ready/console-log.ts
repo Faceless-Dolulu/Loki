f4e6ed8e48f5b7d7e7fc51074bf5bef7d2e179e4
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-01-01 18:07:53 -0500

    Bot has activity set on startup

--- src/events/ready/console-log.ts
+++ src/events/ready/console-log.ts
@@ -1,6 +1,20 @@
-import type { Client } from 'discord.js';
-import type { CommandKit } from 'commandkit';
- 
-export default function (c: Client<true>, client: Client<true>, handler: CommandKit) {
-    console.log(`${c.user.username} is ready!`);
-};
+import { ActivityType, PresenceUpdateStatus, type Client } from "discord.js";
+import type { CommandKit } from "commandkit";
+
+export default function (
+	c: Client<true>,
+	client: Client<true>,
+	handler: CommandKit
+) {
+	console.log(`${c.user.username} is ready!`);
+
+	client.user.setPresence({
+		activities: [
+			{
+				name: `over ${client.guilds.cache.size} servers.`,
+				type: ActivityType.Watching,
+			},
+		],
+		status: PresenceUpdateStatus.Online,
+	});
+}
