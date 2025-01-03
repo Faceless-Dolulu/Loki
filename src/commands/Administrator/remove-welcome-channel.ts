6ae157e347d70325122f7feddc2a2c2e1461bd6d
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-01-03 18:53:51 -0500

    Bot will now monitor # of servers joined in it's custom status

--- src/commands/Administrator/remove-welcome-channel.ts
+++ src/commands/Administrator/remove-welcome-channel.ts
@@ -1,45 +1,54 @@
-import { ChannelType, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import {
+	ChannelType,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+} from "discord.js";
 import { SlashCommandProps } from "commandkit";
-import welcomeChannelSchema from '../../models/WelcomeChannel.js';
+import welcomeChannelSchema from "../../models/WelcomeChannel.js";
 
 export const data = new SlashCommandBuilder()
-.setName('welcome-remove')
-.setDescription('Removes a welcome channel')
-.setDMPermission(false)
-.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-.addChannelOption( option =>
-    option
-    .setName('target-channel')
-    .setDescription('The channel to remove welcome messages from')
-    .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
-    .setRequired(true)
-);
+	.setName("welcome-remove")
+	.setDescription("Removes a welcome channel")
+	.setDMPermission(false)
+	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+	.addChannelOption((option) =>
+		option
+			.setName("target-channel")
+			.setDescription("The channel to remove welcome messages from")
+			.addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
+			.setRequired(true)
+	);
 
-export async function run ({ interaction, client, handler}: SlashCommandProps) {
-    try {
-        const welcomeChannel = interaction.options.getChannel('target-channel');
-        await interaction.deferReply({ephemeral: true});
+export async function run({ interaction, client, handler }: SlashCommandProps) {
+	try {
+		const welcomeChannel = interaction.options.getChannel("target-channel");
+		await interaction.deferReply({ ephemeral: true });
 
-        const query = {
-            guildId: interaction.guildId,
-            channelId: welcomeChannel?.id,
-        };
+		const query = {
+			guildId: interaction.guildId,
+			channelId: welcomeChannel?.id,
+		};
 
-        const channelExistsInDb = await welcomeChannelSchema.exists(query);
+		const channelExistsInDb = await welcomeChannelSchema.exists(query);
 
-        if (!channelExistsInDb) {
-            interaction.followUp('That channel has not been configured for welcome messages.');
-            return;
-        }
-        welcomeChannelSchema.findOneAndDelete(query)
-        .then(()=> {
-            interaction.followUp(`Removed ${welcomeChannel} from receving welcome messages.`);
-        }).catch( (error) => {
-            interaction.followUp(`Database error. Please try again in a moment.`);
-            console.log(`DB error in ${__filename}:\n`, error);
-        })
-    } catch (error) {
-        console.log(`Error in ${__filename}:\n`, error);
-    }
-
+		if (!channelExistsInDb) {
+			interaction.followUp(
+				"That channel has not been configured for welcome messages."
+			);
+			return;
+		}
+		welcomeChannelSchema
+			.findOneAndDelete(query)
+			.then(() => {
+				interaction.followUp(
+					`Removed ${welcomeChannel} from receving welcome messages.`
+				);
+			})
+			.catch((error) => {
+				interaction.followUp(`Database error. Please try again in a moment.`);
+				console.log(`DB error in ${__filename}:\n`, error);
+			});
+	} catch (error) {
+		console.log(`Error in ${__filename}:\n`, error);
+	}
 }
